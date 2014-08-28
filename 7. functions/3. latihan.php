f33b82ab256606b320f0bb43b924bb254d600e20
Akku Iki Alie <ali@firzil.co.id>
2014-08-28 10:09:26 +0700

    finish latihan 3 in functions 7

--- 7. functions/3. latihan.php
+++ 7. functions/3. latihan.php
@@ -6,7 +6,12 @@ var_dump(ucwords('science afandi'));
 // - tidak boleh memanggil ucwords
 function dda_ucwords($nama)
 {
-
+   $out =array();
+   $getWord = explode(' ', $nama);
+   for($i = 0; $i<count($getWord);$i++) {
+      $out[] = ucfirst($getWord[$i]);
+   }
+   return implode(" ", $out);
 }
 
 var_dump(dda_ucwords('science afandi'));
