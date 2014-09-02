0f344c05baef727f2c16e1765facdaf99782f5b2
Akku Iki Alie <ali@firzil.co.id>
2014-09-02 22:17:36 +0700

    Finish tugas 8

--- 8. data structure/2. object/tugas2/bantal.php
+++ 8. data structure/2. object/tugas2/bantal.php
@@ -1,27 +1,40 @@
 <?php
 
 class Bantal {
-    private $apakahKotor;
-    private $jumlahPemakaian = 3;
+    private $apakahKotor = false;
+    private $jumlahPemakaian = 5;
     
+    public function maxPakai($maxPakai) {
+        $this->jumlahPemakaian = $maxPakai;
+        echo "Max Pakai : ". $this->jumlahPemakaian."<br>";
+    }
+
     public function pakai() {
-///    	if($this->jumlahPemakaian ) {
-//	   echo $
-//	}
-        --$this->jumlahPemakaian;
+        if($this->jumlahPemakaian > 0) {
+            echo "Siap Pakai<br>";
+            --$this->jumlahPemakaian;
+        } else {
+            echo "Maaf, sudah waktunya untuk di cuci<br>";
+            $this->apakahKotor = true;
+        }
     }
     
-    public function cekPemakaian() {
-        $respond = "Jumlah Pemakaian saat ini : ".$this->jumlahPemakaian;
-	return $respond;
+    public function cuci() {
+        if($this->apakahKotor) {
+            echo "Bantal sedang di cuci<br>";
+        } else {
+            echo "Bantal sudah di cuci<br>";
+        }
+
     }
 }
 
 
 $bantal = new Bantal();
+$bantal->maxPakai(2);
 $bantal->pakai();
 $bantal->pakai();
 $bantal->pakai();
-//$bantal->cuci();
+$bantal->cuci();
+// $bantal->maxPakai(1);
 $bantal->pakai();
-echo $bantal->cekPemakaian();
