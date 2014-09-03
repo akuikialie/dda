80b79bd33435cf6f6a933648af98be2e5e439991
ali@firzil.co.id <achmadali1992@yahoo.com>
2014-09-03 09:46:25 +0700

    update tugas lagi

--- 8. data structure/2. object/tugas2/galon.php
+++ 8. data structure/2. object/tugas2/galon.php
@@ -2,11 +2,34 @@
 
 class Galon {
 
-    
+    public $adaGalon;
+    private $jumlahAir = 4;
+    private $totalAir;
+
+    public function diMinum($minum)
+    {
+        if ($this->totalAir < 0) {
+            echo "Maaf air Galon sudah habis<br>";
+        } else {
+            $this->jumlahAir = $this->jumlahAir - (float) $minum;
+
+            $this->totalAir = $this->adaGalon * $this->jumlahAir;
+            echo " Air galon tersisa : " . $this->totalAir . "<br>";
+        }
+    }
+
+    public function cekAirGalon()
+    {
+        return $this->totalAir < 0 ? "Habis" : "Masih (" . $this->totalAir . ")";
+    }
+
 }
 
 $g = new \Galon();
-
-
-
-var_dump($g);
+$g->adaGalon = 1;
+$g->diMinum(0.5);
+$g->diMinum(0.7);
+$g->diMinum(1.5);
+$g->diMinum(1.5);
+$g->diMinum(1.5);
+echo "Apakah air galon masih ada : " . $g->cekAirGalon() . "<br>";
