3027ed457cf818c70a9488f503669dbea4484cf0
ali@firzil.co.id <achmadali1992@yahoo.com>
2014-09-03 08:30:53 +0700

    Finish tugas lantai

---
+++ 8. data structure/2. object/tugas2/lantai.php
@@ -0,0 +1,32 @@
+<?php
+
+class lantai {
+
+    public $luasLantai;
+    public $ukuranPanjangKeramik;
+    public $ukuranLebarKeramik;
+    private $jumlahKeramik;
+    private $luasTiapKeramik;
+    
+
+    public function hitungJumlahKeramikYangDibutuhkan()
+    {
+        $this->luasTiapKeramik = $this->cmToM($this->ukuranPanjangKeramik) * $this->cmToM($this->ukuranLebarKeramik);
+        echo "Luas Tiap Keramik : ". (float)$this->luasTiapKeramik."m2<br>";
+        echo "Luas Lantai : ".(float)$this->luasLantai."m2<br>";
+        $hitung = $this->luasLantai / $this->luasTiapKeramik;
+        echo "Jumlah Keramik : ".round($hitung)."";
+    }
+
+    private function cmToM($cm)
+    {
+        return $cm/100;
+    }
+
+}
+
+$l = new \Lantai();
+$l->luasLantai = 1; //satuan meter kubik
+$l->ukuranPanjangKeramik = 34; //satuan centimeter 
+$l->ukuranLebarKeramik = 34; //satian centimeter
+$l->hitungJumlahKeramikYangDibutuhkan();
