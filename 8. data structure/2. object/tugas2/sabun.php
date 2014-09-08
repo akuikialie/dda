7589b27cbe3dd0dd6e7b4cb4249876e9749bb700
ali@firzil.co.id <achmadali1992@yahoo.com>
2014-09-09 02:47:52 +0700

    update tugas

--- 8. data structure/2. object/tugas2/sabun.php
+++ 8. data structure/2. object/tugas2/sabun.php
@@ -1,8 +1,18 @@
 <?php
 
 class Sabun {
+    
+    private $apakahBersih = false;
 
-
+    public function cuci() {
+        if($this->apakahBersih) {
+	}
+    }
 }
 
 $s = new Sabun();
+$s->jenisSabun = 'Tangan';
+$s->apakahTanganBersih();
+$s->cuciTangan();
+$s->apakahTanganBersih();
+
